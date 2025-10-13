a2bbba9901eaf9881a5cf89db25050f7c2c391e7
NothVarit <157122937+NothVarit@users.noreply.github.com>
2025-10-13 19:39:51 +0700

    fix: User model

--- models/User.js
+++ models/User.js
@@ -5,6 +5,31 @@ const UserSchema = new mongoose.Schema({
         type: String,
         require: true
     },
+    email: {
+        type: String,
+        required: [true, "Please add an email"],
+        unique: true,
+        match: [
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          "Please add a valid email",
+        ],
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user",
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+        minlength: 6,
+        select: false,
+    },
+    telephoneNumber: {
+        type: String,
+        required: [true, "Please add phoneNumber"],
+        match: [/^0[689]\d{8}$/, "Please enter a valid Thai phone number"],
+    },
     displayName: {
         type: String,
         require: true
