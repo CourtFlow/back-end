b30892ef5d8853ea37464b5e7d6b7e57113215b9
NothVarit <157122937+NothVarit@users.noreply.github.com>
2025-10-13 20:35:29 +0700

    fix: routes auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,16 @@
-const express = require('express')
-const passport = require('passport')
-const router = express.Router()
+const express = require("express");
+const { register, login, getMe, logout, updateUser} = require("../controllers/auth");
+const { protect, authorize } = require("../middleware/auth");
+const { verifyEmail } = require("../controllers/auth");
 
-// @desc Auth with google
-// @route GET /auth/google
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
+const router = express.Router();
 
-// @desc Google auth callback
-// @route GET /auth/google/callback
-router.get('/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/' }), 
-  (req, res) => {
-    res.json({
-        message: 'Google authentication successful!',
-        user: req.user, // Passport attaches the authenticated user here
-      })
-  }
-)
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", protect, getMe);
+router.get("/logout", logout);
+router.get("/verify-email", verifyEmail);
 
-module.exports = router
+router.route('/:id').put(protect, authorize("user", "admin"), updateUser);
+
+module.exports = router;   
